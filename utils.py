46fcf5679050c5ee542b20a8cc44f170ad19a7d5
Miracle Ikechukwu <miracleboy4gold@gmail.com>
2024-02-23 13:40:51 +0100

    Implemented image splitting utility

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 def one_hot_encode(x, num_classes):
     """
         One hot encode the target variable.
@@ -10,3 +9,13 @@ def one_hot_encode(x, num_classes):
     x_[:, :2] *= 0.0
     x_[range(x.shape[0]), num_classes] = x.max()
     return x_
+
+
+def split_image(img):
+    height, width, channel = img.shape
+    half_width = width // 2
+
+    data = img[:, :half_width]
+    label = img[:, half_width:]
+
+    return data, label
